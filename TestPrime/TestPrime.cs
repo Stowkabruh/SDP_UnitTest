53dbe190c7a4344d5d8c2ca1d92bab86c1a55803
Stowkabruh <reggaenerator2000@gmail.com>
2022-02-03 12:40:36 +0100

    Homework

---
+++ TestPrime/TestPrime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using CheckPrime;
+
+namespace TestPrime
+{
+    public class TestPrime
+    {
+        [Test]
+        public void NegativeValueException()
+        {
+            Assert.Throws<ArgumentException>(() => CheckPrimes.IsPrime(-5));
+        }
+
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        [TestCase(6, false)]
+        public void ValueIsPrime(int value, bool expectedValue)
+        {
+            // act
+            var actualValue = CheckPrimes.IsPrime(value);
+
+            // assert
+            actualValue.Should().Be(expectedValue);
+        }
+    }
+}
